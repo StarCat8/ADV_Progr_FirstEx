c7eec7dccb541252afeac8fdaba0085a3c866f2e
Lapo <129602508+StarCat8@users.noreply.github.com>
2023-06-04 17:01:02 +0200

    Add files via upload

---
+++ RUN.sh
@@ -0,0 +1,16 @@
+mkdir build
+mv allvars.c build/
+mv allvars.h build/
+mv funzioni.c build/
+mv funzioni.h build/
+make
+echo -e "\n"
+echo $?
+echo "---------------------------------------|"
+echo "                                       |"
+echo "                                       |"
+echo "       build folder creater            |"
+echo "                                       |"
+echo "                                       |"
+echo "---------------------------------------|"
+
